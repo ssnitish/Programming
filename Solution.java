99ed57a191f1efc9bd72a6141e0f7a60be27f5e1
ssnitish <ssnitish0000@gmail.com>
2017-04-14 22:32:35 +0530

    first commit

---
+++ Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public void printHellow() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		
+	}
+	
+	public int sum(int a, int b){
+		return a+b;
+	}
+	
+	
+	
+
+}
---
+++ Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+	
+	public static void main(String[] args){
+		System.out.println(new Solution().sum(4,5));
+	}
+
+}
